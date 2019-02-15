b3ca6088cdade79348e94a92860a7905956a8eda
Charles Boadu <ekowcharles@gmail.com>
2019-02-14 23:31:39 -0600

    Add web server application that listens on port 8993

---
+++ ping.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	defer fmt.Println("Exited!")
+
+	version, err := ioutil.ReadFile(".version")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, rq *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, fmt.Sprintf("pong %s", string(version)))
+	})
+
+	log.Fatal(http.ListenAndServe(":8993", nil))
+}
